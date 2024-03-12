7d1966038b3286aa7f6f4303301fb3c0f558021d
Junhao Liao <junhao@junhao.ca>
2024-03-11 21:29:32 -0400

    Add TypeScript support to ESLint configuration `backend-ts`.

--- backend-ts.js
+++ backend-ts.js
@@ -5,9 +5,27 @@ module.exports = {
     extends: [
         "./common",
     ],
+    plugins: [
+        "@typescript-eslint",
+    ],
+    parser: "@typescript-eslint/parser",
+    settings: {
+        "import/resolver": {
+            typescript: {},
+        },
+    },
     rules: {
         "no-console": [
             "error",
         ],
+        "no-restricted-syntax": "off",
+        "no-underscore-dangle": [
+            "error", {
+                allow: [
+                    "_id",
+                ],
+            },
+        ],
     },
 };
+
